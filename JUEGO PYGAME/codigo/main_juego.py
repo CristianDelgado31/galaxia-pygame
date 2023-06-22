65253c88c6f1fbe12dc9ff9385e0980c0612a7dc
CristianDelgado31 <cristian-31-10@hotmail.com>
2023-06-21 23:03:01 -0300

    .

--- JUEGO PYGAME/codigo/main_juego.py
+++ JUEGO PYGAME/codigo/main_juego.py
@@ -115,19 +115,6 @@ while True:
                     lista_nombre[:] = []
                     #####################################################################
                     mostrar_datos_ranking(lista_nombre, lista_puntajes)                                        
-                    #ordena bien los puntajes de mayor a menor
-                    if len(lista_puntajes) >= 2:
-                        for i in range(len(lista_puntajes)-1):
-                            for j in range(i+1, len(lista_puntajes)):
-                                if lista_puntajes[i] < lista_puntajes[j]:
-                                    aux_puntaje = lista_puntajes[i]
-                                    lista_puntajes[i] = lista_puntajes[j]
-                                    lista_puntajes[j] = aux_puntaje
-
-                                    aux_nombre = lista_nombre[i]
-                                    lista_nombre[i] = lista_nombre[j]
-                                    lista_nombre[j] = aux_nombre
-
 
                     #actualizar_ranking
                     ventana_ranking.nombre_primer_puesto = lista_nombre[0]
